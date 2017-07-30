b225f066b6ff2d0923b4de6b2389fde9f41fc00d
xuyunfeng <15801394740@163.com>
2017-07-31 03:42:39 +0800

    first commit

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>I am goods</div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,30 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+import goods from 'components/goods/goods'
+import ratings from 'components/ratings/ratings'
+import seller from 'components/seller/seller'
+
+import 'common/stylus/index.styl'
+
+Vue.use(VueRouter)
+
+let app = Vue.extend(App)
+
+let router = new VueRouter({
+  routes: [
+    {path: '/goods', component: goods},
+    {path: '/ratings', component: ratings},
+    {path: '/seller', component: seller}
+  ],
+  linkActiveClass: 'active'
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router: router,
+  render: h => h(app)
+})
